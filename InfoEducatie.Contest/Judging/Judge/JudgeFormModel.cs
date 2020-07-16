b59495b9ae6882c36eec4fe25514b8d36ec6bb31
TDR <tdr@tdrs.ro>
2020-07-16 23:09:07 +0300

    add judge by email

--- InfoEducatie.Contest/Judging/Judge/JudgeFormModel.cs
+++ InfoEducatie.Contest/Judging/Judge/JudgeFormModel.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using InfoEducatie.Contest.Categories;
 using MCMS.Base.Data.FormModels;
+using MCMS.Base.SwaggerFormly.Formly;
 using MCMS.Base.SwaggerFormly.Formly.Fields;
 
 namespace InfoEducatie.Contest.Judging.Judge
@@ -10,5 +11,10 @@ namespace InfoEducatie.Contest.Judging.Judge
         [Required]
         [FormlySelect(typeof(CategoriesAdminApiController))]
         public CategoryViewModel Category { get; set; }
+
+        [Required]
+        [FormlyFieldProp("description", "Existing account email address.", true)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
     }
 }
--- InfoEducatie.Contest/Judging/Judge/JudgesAdminApiController.cs
+++ InfoEducatie.Contest/Judging/Judge/JudgesAdminApiController.cs
@@ -2,8 +2,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using InfoEducatie.Contest.Categories;
+using MCMS.Base.Auth;
 using MCMS.Controllers.Api;
 using MCMS.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.EntityFrameworkCore;
@@ -33,6 +35,37 @@ namespace InfoEducatie.Contest.Judging.Judge
             var all = await Repo.GetAll();
             var allVm = all.Select(e => new JudgeViewModel {FullName = e.FullName, Id = e.Id});
             return Ok(allVm);
+        }
+
+        public override async Task<ActionResult<JudgeFormModel>> Create(JudgeFormModel fm)
+        {
+            var user = await ServiceProvider.GetService<IRepository<User>>()
+                .GetOne(u => u.NormalizedEmail == fm.Email.ToUpper());
+            if (user == null)
+            {
+                return BadRequest($"There is no user with email '{fm.Email}'");
+            }
+
+            if (await Repo.Any(j => j.User == user))
+            {
+                return BadRequest($"This user is already a judge.");
+            }
+
+            var userManager = ServiceProvider.GetService<UserManager<User>>();
+            if (!await userManager.IsInRoleAsync(user, "Jury"))
+            {
+                await userManager.AddToRoleAsync(user, "Jury");
+            }
+
+            var e = new JudgeEntity
+            {
+                Category = ServiceProvider.GetService<IRepository<CategoryEntity>>()
+                    .Attach(new CategoryEntity {Id = fm.Category.Id}),
+                User = user
+            };
+            e = await Repo.Add(e);
+            var vm = MapF(e);
+            return Ok(vm);
         }
     }
 }
