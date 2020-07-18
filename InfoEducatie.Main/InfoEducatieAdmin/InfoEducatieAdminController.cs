de5445eb4e3062718dda2b6738310d53c049ac88
TDR <tdr@tdrs.ro>
2020-07-18 20:11:19 +0300

    clear db admin option

--- InfoEducatie.Main/InfoEducatieAdmin/InfoEducatieAdminController.cs
+++ InfoEducatie.Main/InfoEducatieAdmin/InfoEducatieAdminController.cs
@@ -1,7 +1,14 @@
+using System.Linq;
 using System.Threading.Tasks;
+using InfoEducatie.Contest.Participants.Participant;
+using InfoEducatie.Contest.Participants.Project;
+using MCMS.Base.Auth;
 using MCMS.Controllers.Ui;
+using MCMS.Data;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace InfoEducatie.Main.InfoEducatieAdmin
 {
@@ -16,6 +23,30 @@ namespace InfoEducatie.Main.InfoEducatieAdmin
         public IActionResult Import()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult DeleteParticipants()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("DeleteParticipants")]
+        public async Task<IActionResult> DeleteParticipantsConfirmed()
+        {
+            var participantsRepo = ServiceProvider.GetService<IRepository<ParticipantEntity>>();
+            var projectsRepo = ServiceProvider.GetService<IRepository<ProjectEntity>>();
+            await participantsRepo.Delete(p => true);
+            await projectsRepo.Delete(p => true);
+
+
+            var uService = ServiceProvider.GetService<IRepository<User>>();
+            var userManager = ServiceProvider.GetService<UserManager<User>>();
+            var users = await userManager.GetUsersInRoleAsync("Participant");
+            uService.DbSet.RemoveRange(users);
+            await uService.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
