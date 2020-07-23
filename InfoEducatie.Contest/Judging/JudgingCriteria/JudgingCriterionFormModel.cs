1543dc129dc3ef2a331b94b8d3375029e73b2141
TDR <tdr@tdrs.ro>
2020-07-23 07:44:46 +0300

    judging criteria hotfix

--- InfoEducatie.Contest/Judging/JudgingCriteria/JudgingCriterionFormModel.cs
+++ InfoEducatie.Contest/Judging/JudgingCriteria/JudgingCriterionFormModel.cs
@@ -11,7 +11,7 @@ namespace InfoEducatie.Contest.Judging.JudgingCriteria
     {
         [Required] public string Name { get; set; }
 
-        [Required] [FormlyCkEditor] public string Description { get; set; }
+        [FormlyCkEditor] public string Description { get; set; }
 
         [Required] public int MaxPoints { get; set; }
 
