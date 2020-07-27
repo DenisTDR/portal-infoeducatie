d681b730ee16660b3cae5fed0005dd6726968a12
TDR <tdr@tdrs.ro>
2020-07-27 15:52:42 +0300

    custom activation mails

--- InfoEducatie.Main/InfoEducatieAdmin/InfoEducatieAdminApiController.cs
+++ InfoEducatie.Main/InfoEducatieAdmin/InfoEducatieAdminApiController.cs
@@ -47,12 +47,22 @@ namespace InfoEducatie.Main.InfoEducatieAdmin
             var participants = await participantsRepo.GetAll(p => !p.ActivationEmailSent);
             foreach (var participantEntity in participants)
             {
-                await authService.SendActivationEmail(participantEntity.User, Url, Request.Scheme);
+                await authService.SendActivationEmail(participantEntity.User, Url, Request.Scheme, "InfoEducație 2020 - Important", EmailBody);
                 participantEntity.ActivationEmailSent = true;
                 await participantsRepo.SaveChanges();
             }
 
             return Ok();
         }
+
+        private static readonly string EmailBody = @"<p>Salut, {{fullName}}!</p>
+<p>Primești acest email pentru că te-ai &icirc;nscris la ediția 2020, etapa națională, a <strong>InfoEducație</strong>.</p>
+<p>Informațiile despre desfășurare le poți găsi pe Portalul concursului (program, seminarii, etc).</p>
+<p>Pentru a avea acces la portal trebuie să-ți activezi contul folosind acest <a href='{{activationUrl}}' target='_blank'>link</a> (care a fost generat be baza adresei de e-mail cu care te-ai &icirc;nscris).</p>
+<p>De asemenea, este foarte important să intri pe serverul de <a href='https://discord.gg/Fn5C4Wy'><strong>Discord</strong> al concursului</a>, unde se vor desfășura discuțiile și prezentarea/jurizarea efectivă a proiectelor.</p>
+<p>Prima activitate este discuția cu comisia și va avea loc în această seară (luni, 27 iulie). Poți vedea mai multe detalii pe portal.</p>
+<p>Mulțumim,<br />
+Echipa <strong>InfoEducație</strong></p>
+";
     }
 }
