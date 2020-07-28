f37714516640f50ca449433ccc8dc3519d5062e4
TDR <tdr@tdrs.ro>
2020-07-28 18:38:30 +0300

    improved judges list + added email check column

--- InfoEducatie.Contest/Judging/Judges/JudgeEntity.cs
+++ InfoEducatie.Contest/Judging/Judges/JudgeEntity.cs
@@ -12,7 +12,9 @@ namespace InfoEducatie.Contest.Judging.Judges
     {
         public User User { get; set; }
         [Required] public CategoryEntity Category { get; set; }
-        [NotMapped] public string FullName => User?.FullName ?? "invalid";
+        [NotMapped] public string FullName => User?.FullName ?? "-invalid-";
+        [NotMapped] public bool EmailConfirmed => User?.EmailConfirmed ?? false;
+        [NotMapped] public string Email => User?.Email ?? "-invalid-";
         public JudgeType AvailableFor { get; set; }
 
         public override string ToString()
