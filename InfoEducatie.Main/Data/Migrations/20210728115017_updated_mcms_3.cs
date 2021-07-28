1aa9a1f65f8c709ff10005edd55346e17b6ffaad
Denis TDR <denis@devplant.ro>
2021-07-28 14:50:45 +0300

    updated MCMS

---
+++ InfoEducatie.Main/Data/Migrations/20210728115017_updated_mcms_3.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InfoEducatie.Main.Data.Migrations
+{
+    public partial class updated_mcms_3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "Size",
+                table: "Files",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Size",
+                table: "Files");
+        }
+    }
+}
