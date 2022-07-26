c46ffcbb8281cc64a9b113d851bc39b10148fdf3
TDR <tdr@tdrs.ro>
2022-07-26 15:29:30 +0300

    fixed Colca's typo

--- InfoEducatie.Contest/Exports/ExportHelper.cs
+++ InfoEducatie.Contest/Exports/ExportHelper.cs
@@ -10,7 +10,7 @@ public class ExportHelper
         worksheet.Cell("A2").Value = "Inspectoratul Școlar Județean Vrancea";
         worksheet.Cell("A3").Value = "InfoEducație - Olimpiada de inovare și creație digitală";
         worksheet.Cell("A4").Value = "Ediția 2022, Etapa Națională";
-        worksheet.Cell("A5").Value = "Focșani 25- 30 iulie 2022";
+        worksheet.Cell("A5").Value = "Focșani 25 - 30 iulie 2022";
         worksheet.Range("A1:A5").Style.Font.Bold = true;
     }
 }
