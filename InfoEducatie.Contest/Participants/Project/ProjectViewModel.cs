b07ba19cabeec8a6d2d6744880ef544429e326ea
TDR <tdr@tdrs.ro>
2022-07-28 18:41:55 +0300

    added id in project view model

--- InfoEducatie.Contest/Participants/Project/ProjectViewModel.cs
+++ InfoEducatie.Contest/Participants/Project/ProjectViewModel.cs
@@ -14,26 +14,46 @@ namespace InfoEducatie.Contest.Participants.Project
     [DisplayName("Project")]
     public class ProjectViewModel : ViewModel
     {
+        [TableColumn(Invisible = true)]
+        [DetailsField]
+        public override string Id { get; set; }
+
         [JsonConverter(typeof(ToStringJsonConverter))]
         [TableColumn(DbColumn = "Category.Name")]
+        [DetailsField]
+
         public CategoryViewModel Category { get; set; }
 
-        [TableColumn] public string Title { get; set; }
-        public string Description { get; set; }
-        public string Technologies { get; set; }
-        public string SystemRequirements { get; set; }
+        [TableColumn] [DetailsField] public string Title { get; set; }
+        [DetailsField] public string Description { get; set; }
+        [DetailsField] public string Technologies { get; set; }
+        [DetailsField] public string SystemRequirements { get; set; }
 
         [TableColumn(Orderable = ServerClient.None)]
+        [DetailsField]
+
         public string SourceUrl { get; set; }
 
-        public string Homepage { get; set; }
-        public string OldPlatformId { get; set; }
-        public string DiscourseUrl { get; set; }
-        [TableColumn] public bool IsInOpen { get; set; }
-        [TableColumn(Invisible = true)] public float ScoreProject { get; set; }
-        [TableColumn(Invisible = true)] public float ScoreOpen { get; set; }
-        [TableColumn(Invisible = true)] public float TotalScore => ScoreProject + ScoreOpen;
-        [TableColumn(Invisible = true)] public string FinalPrize { get; set; }
+        [DetailsField] public string Homepage { get; set; }
+        [DetailsField] public string OldPlatformId { get; set; }
+        [DetailsField] public string DiscourseUrl { get; set; }
+        [DetailsField] [TableColumn] public bool IsInOpen { get; set; }
+
+        [TableColumn(Invisible = true)]
+        [DetailsField]
+        public float ScoreProject { get; set; }
+
+        [TableColumn(Invisible = true)]
+        [DetailsField]
+        public float ScoreOpen { get; set; }
+
+        [TableColumn(Invisible = true)]
+        [DetailsField]
+        public float TotalScore => ScoreProject + ScoreOpen;
+
+        [TableColumn(Invisible = true)]
+        [DetailsField]
+        public string FinalPrize { get; set; }
 
         [DetailsField(Hidden = true)]
         [JsonIgnore]
@@ -43,6 +63,8 @@ namespace InfoEducatie.Contest.Participants.Project
             DbFuncFormat = "{0}.Any(p=> <condition>)<sel>MDbFunctions.Concat(p.User.FirstName, ' ', p.User.LastName)",
             Orderable = ServerClient.None)]
         [DisplayName("Participants")]
+        [DetailsField]
+
         public string ProjectsNames => Participants?.Count is { } nr && nr > 0
             ? string.Join(", ", Participants.Select(p => p.FullName))
             : "--";
