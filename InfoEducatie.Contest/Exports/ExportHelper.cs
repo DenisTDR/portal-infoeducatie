b43cc61381db7f7aaed220749386850c76c4e2ec
TDR <tdr@tdrs.ro>
2023-07-24 22:15:01 +0300

    updated header to 2023

--- InfoEducatie.Contest/Exports/ExportHelper.cs
+++ InfoEducatie.Contest/Exports/ExportHelper.cs
@@ -9,8 +9,8 @@ public class ExportHelper
         worksheet.Cell("A1").Value = "Ministerul Educației Naționale";
         worksheet.Cell("A2").Value = "Inspectoratul Școlar Județean Vrancea";
         worksheet.Cell("A3").Value = "InfoEducație - Olimpiada de inovare și creație digitală";
-        worksheet.Cell("A4").Value = "Ediția 2022, Etapa Națională";
-        worksheet.Cell("A5").Value = "Focșani 25 - 30 iulie 2022";
+        worksheet.Cell("A4").Value = "Ediția 2023, Etapa Națională";
+        worksheet.Cell("A5").Value = "Focșani 24 - 29 iulie 2023";
         worksheet.Range("A1:A5").Style.Font.Bold = true;
     }
 }
