627b0d458001ad3c94daab81bc649ac468dbf734
TDR <tdr@tdrs.ro>
2024-08-02 09:01:35 +0300

    updated export xlsx header

--- InfoEducatie.Contest/Exports/ExportHelper.cs
+++ InfoEducatie.Contest/Exports/ExportHelper.cs
@@ -10,7 +10,7 @@ public class ExportHelper
         worksheet.Cell("A2").Value = "Inspectoratul Școlar Județean Vrancea";
         worksheet.Cell("A3").Value = "InfoEducație - Olimpiada de inovare și creație digitală";
         worksheet.Cell("A4").Value = "Ediția 2024, Etapa Națională";
-        worksheet.Cell("A5").Value = "Focșani 29 iulie - 2 august 2024";
+        worksheet.Cell("A5").Value = "Focșani 29 iulie - 3 august 2024";
         worksheet.Range("A1:A5").Style.Font.Bold = true;
     }
 }
